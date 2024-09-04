168069078508089d6d2ab2cc04e9a6962d9c12a5
Maxim <mystic189123@gmail.com>
2024-09-04 14:28:26 +0300

    Projekti

---
+++ main.js
@@ -0,0 +1,5 @@
+function Test(){
+    document.getElementById("Error").innerHTML = "Täytä kaikki kentät"
+    document.getElementById("Error").style.background = "#a00707" 
+    document.getElementById("Error").style.color = "#ffffff"
+}
